e77791cdd767dd51ea6617629484a2b5bb15c9b3
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-20 17:54:33 +0200

    feat: now reading and displaying input data

--- src/bin/part1.rs
+++ src/bin/part1.rs
@@ -1,3 +1,11 @@
-fn main() {
+use aoc_2024_day_20::maze::{BadDataError, Maze};
+
+fn main() -> Result<(), BadDataError> {
     println!("Working on part 1...");
+
+    let input_string = include_str!("../../input/example.txt");
+    let maze = Maze::try_from(input_string)?;
+    println!("{}", maze);
+
+    Ok(())
 }
